18c76b684fa98b8fc27ab971a93aadbc18bdac39
胡ovo <mr.huguangjun@gmail.com>
2025-10-26 21:25:52 +0800

    导入数据集&数据预处理

--- predict_boston/linear_regression.py
+++ predict_boston/linear_regression.py
@@ -1,6 +1,15 @@
-#import necessary packages
 import numpy as np
 import json 
-#read training data
-data_path = 'data/housing.data'
-data = np.fromfile(data_path, sep=' ')
+
+def load_data():
+    #read training data
+    data_path = 'data/housing.data'
+    data = np.fromfile(data_path, sep=' ')
+    #reshape data into a 2D array
+    feature_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+    feature_num = len(feature_names)
+    data = data.reshape([data.shape[0] // feature_num, feature_num])
+    #training set ratio
+    ratio = 0.8
+    offset = int(data.shape[0] * ratio)
+    training_data = data[:offset, :]
